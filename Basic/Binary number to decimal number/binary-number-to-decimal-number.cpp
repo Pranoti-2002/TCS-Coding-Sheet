c085047fc60f1f1c2e19201a130b6a79de3a01ba
Pranoti Bartakke <95330658+Pranoti-2002@users.noreply.github.com>
2023-07-27 13:11:52 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Binary number to decimal number/binary-number-to-decimal-number.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int binary_to_decimal(string str) {
+        // Code here.  
+        int num=0; 
+        int power=str.length()-1;
+        for(int i=0; i<str.length(); i++) 
+        {
+            if(str[i]=='1')
+            {
+                num+=pow(2 , power); 
+            }
+            power--;
+        }
+        
+        return num;
+    }  
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        int ans = ob.binary_to_decimal(str);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
