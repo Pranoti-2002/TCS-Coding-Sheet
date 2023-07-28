a11efc3d386870ea3f08b4726cdb62521d8b0151
Pranoti Bartakke <95330658+Pranoti-2002@users.noreply.github.com>
2023-07-28 19:28:00 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Largest prime factor/largest-prime-factor.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public: 
+    long long int largestPrimeFactor(int N){
+        // code here 
+        int res=0; 
+        
+        for(int i=2; i*i<=N; i++)
+        {
+            while(N%i==0)
+            {
+                res=i;
+                N/=i;
+            }
+        } 
+        if(N>1)
+        {
+            return N;
+        }
+        return res; 
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.largestPrimeFactor(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
